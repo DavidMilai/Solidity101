16b4fa1f8e26a1beb4d270e87d1ac13f2fc0ec6c
DavidMilai <davidmilai67@gmail.com>
2022-09-09 13:32:12 +0300

    data types

---
+++ contracts/DataTypes.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+contract DataTypes {
+    string public myString = "Hello world";
+
+    bool public b = true;
+
+    int256 public minInt = type(int256).min; // getting the minimum int
+    int256 public maxInt = type(int256).max; // getting the maximum int
+
+    address public myAddrex = 0x111ceaFFFFCCCC;
+
+    bytes32 public b32 = 0x111ceaFFFFCCCC;
+}
