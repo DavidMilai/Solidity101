9d4d8a8d8e359d54bd6b3930adb6a70bb0447e97
David Milai <davidmilai67@gmail.com>
2022-09-19 11:24:35 +0300

    local variables

--- contracts/StateVariable.sol
+++ contracts/StateVariable.sol
@@ -9,3 +9,11 @@ contract StateVariables {
         uint notStateVariable = 456; // not saved on the blockchain
     }
 }
+
+contract LocalVariables {
+    uint public myUint = 123; // State variable
+
+    function simpleFunction() external {
+        uint notStateVariable = 456; //Local variable
+    }
+}
