54091a461d29ecba14bcb5498beb5649dee93ae6
David Milai <davidmilai67@gmail.com>
2022-09-19 11:49:20 +0300

    default values

---
+++ contracts/DefaultValues.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.7;
+
+contract DefaultValues {
+    bool public b; // default is false
+    uint public u; //default is 0
+    int public i; //default is 0
+
+    address public a; //default is 0x00000000000
+    bytes32 public b32; //default is 0x(64 zeros)
+}
