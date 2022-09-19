3276f683b2826f6455cc69369d9b5469e42aff19
David Milai <davidmilai67@gmail.com>
2022-09-19 11:30:09 +0300

    global variables

---
+++ contracts/GlobalVariables.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.7;
+
+contract GlobalVariables {
+    function globalVar()
+        external
+        view
+        returns (
+            address,
+            uint,
+            uint
+        )
+    {
+        address sender = msg.sender; // address that called the function
+        uint timeStamp = block.timestamp; // time the contract was called
+        uint blockNum = block.number; // block number
+        return (sender, timeStamp, blockNum);
+    }
+}
