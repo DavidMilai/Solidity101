c5d8327ac0e82fc52b28ac0dbb5751c1d3670857
David Milai <davidmilai67@gmail.com>
2022-09-20 16:06:35 +0300

    loops

---
+++ contracts/ForAndWhileLoops.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.7;
+
+contract ForAndWhileLoops {
+    function loops() external pure {
+        for (uint i = 0; i < 10; i++) {
+            //code
+            if (i == 3) {
+                continue;
+                //this block doesnt execute
+            }
+
+            //more code
+            if (i == 3) {
+                break;
+            }
+        }
+
+        uint j = 0;
+        while (j < 10) {
+            j++;
+        }
+    }
+
+
+function sum(uint n) external pure returns (uint){
+    uins s;
+
+    for(uint i;i<=10;i++){
+        s+=i
+    }
+    return s;
+}
+
+
+}
