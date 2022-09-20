33a94dd50818e47892d6c931b42261adf2acdca0
David Milai <davidmilai67@gmail.com>
2022-09-20 15:50:45 +0300

    constants

---
+++ contracts/Constants.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.7;
+
+contract Constants {
+    address public constant MY_ADDRESS = 0x8785695894534245u; //advantage is it uses lower gas cost
+     address public  MY_ADDRESSTWO = 0x8785695894534245u; 
+    int public constant MY_UINT = 123; 
+}
