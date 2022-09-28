f92e80d818d829dd49f98dca13556e05e3ed4868
David Milai <davidmilai67@gmail.com>
2022-09-28 14:58:23 +0300

    returning many

---
+++ contracts/FunctionOutputs.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract FunctionOutputs {
+
+    function returnMany() public pure returns (uint,bool) {
+        return (1,true);
+    }
+
+        function returnManyNamed() public pure returns (uint x,bool b) {
+        x = 1;
+        b = true;
+        // This saves on gas
+    }
+}
