e97ba9c575130eaa5eb0fdbc920dbe90c1a52e23
David Milai <davidmilai67@gmail.com>
2022-09-28 15:03:09 +0300

    destructingAssignment

--- contracts/FunctionOutputs.sol
+++ contracts/FunctionOutputs.sol
@@ -2,14 +2,18 @@
 pragma solidity ^0.8.7;
 
 contract FunctionOutputs {
-
-    function returnMany() public pure returns (uint,bool) {
-        return (1,true);
+    function returnMany() public pure returns (uint, bool) {
+        return (1, true);
     }
 
-        function returnManyNamed() public pure returns (uint x,bool b) {
+    function returnManyNamed() public pure returns (uint x, bool b) {
         x = 1;
         b = true;
         // This saves on gas
+    }
+
+    function destructingAssignment() public pure {
+        (uint y, bool b) = returnManyNamed();
+        (, bool c) = returnManyNamed();
     }
 }
